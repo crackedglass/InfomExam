c6c811adaa58267fee816f3658fa2d2e270f31b6
crackedglass <bunin.basil@yandex.ru>
2021-04-15 20:49:03 +0700

    Make some changes

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from tkinter import * 
+import json
+file = open("label_list.json", "r")
+str1 = file.read()
+a = json.loads(str1)
 root = Tk()
+root.title(a["title"])
+but = Button(root, text = a["button_text"], width = 25, height = 5, bg = "black",fg ="red")
+but.pack()
+
+
 root.mainloop()
